b8985bf65b56e9d40000db58e1645fb97d35f4af
Lisa Walsh <lwalshphotography@yahoo.com>
2022-11-17 17:44:33 -0500

    adding updated .js files

---
+++ index.js
@@ -0,0 +1,85 @@
+//Copied from README challenge. Be sure to change the code accordingly
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require('fs');
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const distpath = path.join(DIST_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+const teammembers = [];
+const idarray = [];
+
+// Inform user of usage
+console.log("Welcome to the Team Profile Generator! Please answer the following questions to generate your team profile.");
+
+// Function to create manager
+function appMenu() {
+    function createManager() {
+        console.log("Please build your team");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's id?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a positive number greater than zero.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email address.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your manager's office number?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a positive number greater than zero.";
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teammembers.push(manager);
+            idarray.push(answers.managerId);
+            createTeam();
+        });
+    }
+    }
